09e0b100369a4bb35019ed0c05c40afad1f90c83
Nodira017 <81599253+Nodira017@users.noreply.github.com>
2021-06-28 16:10:29 +0500

    Add files via upload

---
+++ springmvsapp1/src/main/java/uz/nodira/config/MySpringMVCDispatcherInitializer.java
@@ -0,0 +1,20 @@
+package uz.nodira.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMVCDispatcherInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
